f459ca742f3e340c974a4ae0cdda0c1a13393c24
sonishn213 <sonishn213@gmail.com>
2026-03-11 18:41:48 +0530

    rename resume file

--- src/components/Home/pdfBtn.js
+++ src/components/Home/pdfBtn.js
@@ -1,6 +1,6 @@
 export default function PdfBtn() {
     return (
-        <a href="/files/resume-spring-boot.pdf" target="_blank" rel="noopener noreferrer" className="tj-btn-primary-2">Resume
+        <a href="/files/resume.pdf" target="_blank" rel="noopener noreferrer" className="tj-btn-primary-2">Resume
             <span className="icon_box">
                 <i className="flaticon-download icon_first"></i>
                 <i className="flaticon-download icon_second"></i>
